6e1e7f1733191e9a309b8e6043233db43d9c5d2c
Ali Yara <ali.yara.fr@gmail.com>
2025-11-26 16:02:34 +0100

    update pom.xml & ApiResponse

--- src/main/java/com/aliyara/smartshop/payload/ApiResponse.java
+++ src/main/java/com/aliyara/smartshop/payload/ApiResponse.java
@@ -3,7 +3,7 @@ package com.aliyara.smartshop.payload;
 import java.time.LocalDateTime;
 
 public record ApiResponse<T>(
-        Integer status,
+        boolean status,
         String message,
         LocalDateTime timestamp,
         T data
